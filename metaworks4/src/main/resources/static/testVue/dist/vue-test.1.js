9a635c174f23711031ad57c8e84d269ef9428f69
Yong <lusky87@gmail.com>
2023-05-24 11:12:17 +0900

    commit
    commit

---
+++ metaworks4/src/main/resources/static/testVue/dist/vue-test.1.js
@@ -0,0 +1,31 @@
+(window["vueTest_jsonp"] = window["vueTest_jsonp"] || []).push([[1],{
+
+/***/ "231e":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("8bbf");
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(vue__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("bcd5");
+/* harmony import */ var _vue_web_component_wrapper__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("b803");
+
+
+// import { createApp, h } from "vue";
+// import wrapper from "vue3-webcomponent-wrapper";
+
+
+// const testElement = wrapper(App, createApp, h);
+const testElement = Object(_vue_web_component_wrapper__WEBPACK_IMPORTED_MODULE_2__[/* default */ "a"])(vue__WEBPACK_IMPORTED_MODULE_0___default.a, _App__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"]);
+
+//window.customElements.define('my-custom-element', CustomElement);
+// window.customElements.define('order-form', OrderFormElement);
+// window.customElements.define('vue-component', VueComponentElement);
+window.customElements.define('vue-test', testElement);
+/* harmony default export */ __webpack_exports__["default"] = (testElement);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=vue-test.1.js.map
